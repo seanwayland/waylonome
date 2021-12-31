cc4d43d1612ff22b57695ecc898787bb7a4a20a1
seanwayland <seanwayland@gmail.com>
2021-12-30 23:39:58 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,49 @@
+var metronome = new Metronome();
+var tempo = document.getElementById('tempo');
+tempo.textContent = metronome.tempo;
+
+var accent = document.getElementById('accent');
+accent.textContent = metronome.accent;
+
+var swing = document.getElementById('swing');
+swing.textContent = metronome.swing;
+
+var playPauseIcon = document.getElementById('play-pause-icon');
+
+var playButton = document.getElementById('play-button');
+playButton.addEventListener('click', function() {
+    metronome.startStop();
+
+    if (metronome.isRunning) {
+        playPauseIcon.className = 'pause';
+    }
+    else {
+        playPauseIcon.className = 'play';
+    }
+
+});
+
+var tempoChangeButtons = document.getElementsByClassName('tempo-change');
+for (var i = 0; i < tempoChangeButtons.length; i++) {
+    tempoChangeButtons[i].addEventListener('click', function() {
+        metronome.tempo += parseInt(this.dataset.change);
+        tempo.textContent = metronome.tempo;
+    });
+}
+
+var accentChangeButtons = document.getElementsByClassName('accent-change');
+for (var i = 0; i < accentChangeButtons.length; i++) {
+    accentChangeButtons[i].addEventListener('click', function() {
+        metronome.accent += parseInt(this.dataset.change);
+        accent.textContent = metronome.accent;
+    });
+}
+
+var swingChangeButtons = document.getElementsByClassName('swing-change');
+for (var i = 0; i < swingChangeButtons.length; i++) {
+    swingChangeButtons[i].addEventListener('click', function() {
+        metronome.swing += parseInt(this.dataset.change);
+        swing.textContent = metronome.swing;
+    });
+}
+
